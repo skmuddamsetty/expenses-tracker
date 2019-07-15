c8371ddae60492083576927157a7e4bd04eaa081
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-07-15 18:53:26 -0400

    splitted files into its own components

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const AddExpensePage = () => <div>This is from my Add Expense component</div>;
+export default AddExpensePage;
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const EditExpensePage = () => <div>This is from my Edit Expense component</div>;
+export default EditExpensePage;
