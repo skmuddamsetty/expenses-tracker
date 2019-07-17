674bea396c3ee1aa10a54bbb761f8dbff706ab85
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-07-17 00:24:37 -0400

    connecting expense list to redux store and rendering the expenses

--- src/app.js
+++ src/app.js
@@ -4,17 +4,25 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 import AppRouter from './routers/AppRouter';
+import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import { getVisibleExpenses } from './selectors/expenses';
 
+// Start Dispatching actions from app.js
 const store = configureStore();
 store.dispatch(addExpense({ description: 'Water Bill' }));
-store.dispatch(addExpense({ description: 'Gas Bill' }));
-store.dispatch(setTextFilter('bill'));
-console.log(store.getState());
-console.log(
-  getVisibleExpenses(store.getState().expenses, store.getState().filters)
+// store.dispatch(addExpense({ description: 'Gas Bill' }));
+// store.dispatch(setTextFilter('bill'));
+// console.log(store.getState());
+// console.log(
+//   getVisibleExpenses(store.getState().expenses, store.getState().filters)
+// );
+// End Dispatching actions from app.js
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
 );
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const ExpenseList = props => (
+  <div>
+    <h1>Expenses List</h1>
+    {props.expenses.length}
+  </div>
+);
+// const ConnectedExpenseList = connect(state => {
+//   return {
+//     expenses: state.expenses
+//   };
+// })(ExpenseList);
+// export default ConnectedExpenseList;
+
+// this is the most common pattern used rather than creating a seperate const like above and exporting the constant
+// export default (ConnectedExpenseList = connect(state => {
+//   return {
+//     expenses: state.expenses
+//   };
+// })(ExpenseList));
+
+// this is seperating the params that are being sent to the connect function
+const mapStateToProps = state => {
+  return {
+    expenses: state.expenses
+  };
+};
+
+export default connect(mapStateToProps)(ExpenseList);
